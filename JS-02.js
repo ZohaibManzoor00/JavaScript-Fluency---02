82ac81ad9f983fb5f9e17d532ceb0146caeac3c0
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-23 12:00:23 -0400

    Problem Set 02

---
+++ JS-02.js
@@ -0,0 +1,26 @@
+// Write a JavaScript function to get the last element of an array. 
+const lastElement = (array) => {
+
+}
+
+console.log(lastElement([0, 3, 4, 2, 5, 7])) // 7
+
+
+// Write a JavaScrip function that returns the duplicate value from an array
+const returnDuplicate = (array) => {
+
+}
+
+console.log(returnDuplicate([0, 2, 1, 5, 2])) // 2
+
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+const fizzBuzz = (number) => {
+
+}
+
+console.log(fizzBuzz(5)) // ["1","2","Fizz","4","Buzz"]
